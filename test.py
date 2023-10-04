5ca4532d27dd13e058b28f77c05f14f842c63e59
Jay Morse <jaymorse@Jays-MacBook-Air.local>
2023-10-04 12:36:36 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,32 @@
+from pymodbus.client import ModbusSerialClient
+
+# Define the serial port (ttyUSB0) and baudrate
+ser = ModbusSerialClient(method='rtu', port='/dev/ttyUSB0', baudrate=9600)
+
+# Ensure the client is connected
+if not ser.connect():
+    print("Failed to connect to the Modbus device.")
+else:
+    try:
+        # Specify the slave ID (address)
+        slave_id = 1  # Replace with the actual slave ID of your device
+
+        # Specify the Modbus register to read (e.g., register number 0)
+        register_address = 0
+
+        # Read a single holding register
+        response = ser.read_holding_registers(register_address, 1, unit=slave_id)
+
+        if response.isError():
+            print(f"Error response: {response}")
+        else:
+            # Extract the value from the response
+            value = response.registers[0]
+            print(f"Register {register_address}: {value}")
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+    finally:
+        # Close the Modbus connection
+        ser.close()
