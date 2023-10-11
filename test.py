7627ed6558bc4f8eb7071397325dad19f6f4c6d0
Jay Morse <jaymorse123@gmail.com>
2023-10-11 09:57:46 +0100

    streamlined

--- test.py
+++ test.py
@@ -12,6 +12,10 @@ def write(register_address, value_to_write):
             print(f"Successfully wrote {value_to_write} to register {register_address}")
             print(f"Raw response: {response}")
 
+def degrees_to_steps(degrees):
+    return degrees * 100
+
+#-----------------------------------------------------------------------------------------------------------
 
 # Define the serial port (ttyUSB0) and baudrate
 ser = ModbusSerialClient(method='rtu', port='/dev/ttyUSB0', baudrate=9600, parity='E', stopbits=1, bytestize=8, timeout=5.0)
@@ -21,8 +25,8 @@ if not ser.connect():
     print("Failed to connect to the Modbus device.")
 else:
     try:
-        write(0x1803, 1000)
-        write(0x79, 8)
+        write(0x1803, degrees_to_steps())
+        write(0x79, 8) #This is the START command
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
