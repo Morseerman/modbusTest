f06849fe6dbf19f6a96e8190abf876e231a1865e
Jay Morse <jaymorse123@gmail.com>
2023-10-13 13:51:23 +0100

    small change

--- flaskApplication.py
+++ flaskApplication.py
@@ -1,18 +1,14 @@
 from flask import Flask, request, jsonify, render_template
 from pymodbus.client import ModbusSerialClient
 import time
+import write
 
 app = Flask(__name__)
 app.static_folder = 'static'
 
 ser = ModbusSerialClient(method='rtu', port='/dev/ttyUSB0', baudrate=9600, parity='E', stopbits=1, bytesize=8, timeout=5.0)
 
-def write(register_address, value_to_write):
-    response = ser.write_register(register_address, value_to_write, 15)
-    if response.isError():
-        print(f"Error response: {response}")
-    else:
-        print(f"Successfully wrote {value_to_write} to register {register_address}")
+
 
 def degrees_to_steps(degrees):
     return round(degrees * 100)
@@ -44,8 +40,8 @@ def set_position():
             return jsonify(status="error", message="Failed to connect to the Modbus device.")
 
         # Write the provided angle and execute the START command
-        write(0x1803, degrees_to_steps(degrees))
-        write(0x79, 8) # This is the START command
+        write.write(0x1803, degrees_to_steps(degrees))
+        write.write(0x79, 8) # This is the START command
         
         return jsonify(status="success", message=f"Set motor angle to {degrees} degrees")
 
