9b22e510c8e4b7a2ee46a11eebe9a39ddc2746d8
Morseerman <jaymorse123@gmail.com>
2023-10-20 14:26:20 +0100

    workin

--- Python/Python-WitProtocol/chs/JY901S.py
+++ Python/Python-WitProtocol/chs/JY901S.py
@@ -147,9 +147,9 @@ if __name__ == '__main__':
     )
 
     if (platform.system().lower() == 'linux'):
-        device.serialConfig.portName = "/dev/ttyUSB0"   #设置串口   Set serial port
+        device.serialConfig.portName = "/dev/ttyUSB3"   #设置串口   Set serial port
     else:
-        device.serialConfig.portName = "COM39"          #设置串口   Set serial port
+        device.serialConfig.portName = "COM5"          #设置串口   Set serial port
     device.serialConfig.baud = 9600                     #设置波特率  Set baud rate
     device.openDevice()                                 #打开串口   Open serial port
     readConfig(device)                                  #读取配置信息 Read configuration information
