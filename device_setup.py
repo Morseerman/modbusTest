98303454ecaa164c434ccb3469917cc2c8698534
Jay Morse <jaymorse123@gmail.com>
2023-11-03 09:44:23 +0000

    a

--- device_setup.py
+++ device_setup.py
@@ -6,18 +6,31 @@ import subprocess
 DEVICES_INFO = {
     'motor': {'idVendor': '0403', 'idProduct': '6001', 'serial': 'A60321EB'},
     'compass': {'idVendor': '0403', 'idProduct': '6010', 'serial': 'FTVIZSZU'},
-    'inclinometer': {'idVendor': '1a86', 'idProduct': '7523', 'serial': '0000:01:00.0'}
-
+    'inclinometer': {'idVendor': '1a86', 'idProduct': '7523', 'devpath': '1.1'}
 }
+
 
 UDEV_RULES_PATH = '/etc/udev/rules.d/99-usb-serial.rules'
 
-def create_udev_rule(name, idVendor, idProduct, serial):
-    return (
+def create_udev_rule(name, idVendor, idProduct, devpath=None, serial=None):
+    # Start with the base rule
+    rule = (
         f'SUBSYSTEM=="tty", ATTRS{{idVendor}}=="{idVendor}", '
-        f'ATTRS{{idProduct}}=="{idProduct}", ATTRS{{serial}}=="{serial}", '
-        f'SYMLINK+="{name}"\n'
+        f'ATTRS{{idProduct}}=="{idProduct}"'
     )
+    
+    # Add the serial if available, otherwise use devpath
+    if serial:
+        rule += f', ATTRS{{serial}}=="{serial}"'
+    elif devpath:
+        rule += f', ATTRS{{devpath}}=="{devpath}"'
+    else:
+        raise ValueError("Either serial or devpath must be provided.")
+    
+    # Finish the rule with the symlink
+    rule += f', SYMLINK+="{name}"\n'
+    return rule
+
 
 def write_udev_rules(rules):
     with open(UDEV_RULES_PATH, 'w') as f:
