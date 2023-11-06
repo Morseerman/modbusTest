9ee2e2f8a51588a7026cd91a4cf4d7f30f9d2e6f
jay morse <jaymorse123@gmail.com>
2023-11-06 12:15:47 +0000

    a

--- compass.py
+++ compass.py
@@ -29,6 +29,27 @@ def interpret_direction(compass_data):
         return 'W'
     elif compass_data > 292.5 and compass_data <= 337.5:
         return 'N.W'
+    
+def read_compass_once():
+    global latest_compass_data
+
+    serial_port = '/dev/compass'
+    baud_rate = 4800  
+
+    try:
+        # Open the serial port
+        with serial.Serial(serial_port, baud_rate, timeout=1) as ser:
+            # Read a line of data from the serial port
+            data = ser.readline().decode('utf-8').strip()
+            
+            # Update the latest compass reading
+            latest_compass_data = extract_value_from_data(data)
+            print(latest_compass_data)
+
+    except KeyboardInterrupt:
+        print("Serial communication stopped.")
+    except Exception as e:
+        print(f"Error: {str(e)}")
 
 def read_compass():
     global latest_compass_data
@@ -57,4 +78,4 @@ def get_latest_compass_data():
     return latest_compass_data + f"  ({direction})"
 
 if __name__ == '__main__':
-    read_compass()
+    read_compass_once()
