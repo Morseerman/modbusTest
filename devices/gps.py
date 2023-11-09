6dfb9c4c3f9acf1efb7629e66354f53a98649bee
Jay Morse <jaymorse123@gmail.com>
2023-11-09 14:55:45 +0000

    a

--- devices/gps.py
+++ devices/gps.py
@@ -1,31 +1,30 @@
 import serial
 
-def process_gps_data(data):
-    # Split the string into separate lines
-    lines = data.split('\n')
-    result = {
+def process_gps_data(lines):
+    accumulated_result = {
         "Latitude": "Unknown",
         "Longitude": "Unknown",
         "Altitude": "Unknown",
         "Signal Quality": "Unknown"
     }
-    
+
     for line in lines:
-        # Process $KSXT data for latitude, longitude, and altitude
         if '$KSXT' in line:
             parts = line.split(',')
-            result["Latitude"] = parts[2]
-            result["Longitude"] = parts[3]
-            result["Altitude"] = parts[4] + " meters"
-        
-        # Process #UNIHEADINGA data for signal quality
+            accumulated_result["Latitude"] = parts[2]
+            accumulated_result["Longitude"] = parts[3]
+            accumulated_result["Altitude"] = parts[4] + " meters"
+
         if '#UNIHEADINGA' in line:
             parts = line.split(',')
             sol_status = parts[1]
             pos_type = parts[2] + ', ' + parts[3]
-            result["Signal Quality"] = f"Solution Status: {sol_status}, Position Type: {pos_type}"
+            accumulated_result["Signal Quality"] = f"Solution Status: {sol_status}, Position Type: {pos_type}"
 
-    return result
+    return accumulated_result
+
+
+
 
 serial_port = '/dev/ttyUSB1'
 baud_rate = 921600  
