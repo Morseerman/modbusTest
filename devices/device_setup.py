32afc1d179c02945f826462ae5c2f103dcf701a4
Jay Morse <jaymorse123@gmail.com>
2023-11-29 12:10:08 +0000

    a

--- devices/device_setup.py
+++ devices/device_setup.py
@@ -3,28 +3,19 @@ import subprocess
 
 # Dictionary to hold device information
 DEVICES_INFO = {
-    'motor': {'idVendor': '0403', 'idProduct': '6001', 'serial': 'A60321OT'},
-    'compass': {'idVendor': '0403', 'idProduct': '6010', 'serial': 'FTVIZSZU'},
-    'radio': {'idVendor': '0403', 'idProduct': '6015', 'serial': 'DJ009E6Y'},
-    'inclinometer': {'idVendor': '1a86', 'idProduct': '7523', 'product': 'USB Serial'}
+    'motor': {'idVendor': '0403', 'idProduct': '6001'},
+    'compass': {'idVendor': '0403', 'idProduct': '6010'},
+    'radio': {'idVendor': '0403', 'idProduct': '6015'},
+    'inclinometer': {'idVendor': '1a86', 'idProduct': '7523'}
 }
 
 UDEV_RULES_PATH = '/etc/udev/rules.d/99-usb-serial.rules'
 
-def create_udev_rule(name, idVendor, idProduct, product=None, serial=None):
-    rule_parts = [
-        'SUBSYSTEM=="tty"',
-        f'ATTRS{{idVendor}}=="{idVendor}"',
-        f'ATTRS{{idProduct}}=="{idProduct}"'
-    ]
-    if serial:
-        rule_parts.append(f'ATTRS{{serial}}=="{serial}"')
-    elif product:
-        rule_parts.append(f'ATTRS{{product}}=="{product}"')
-    else:
-        raise ValueError("Either serial or product must be provided.")
-
-    rule = ', '.join(rule_parts) + f', SYMLINK+="{name}"\n'
+def create_udev_rule(name, idVendor, idProduct):
+    rule = (
+        f'SUBSYSTEM=="tty", ATTRS{{idVendor}}=="{idVendor}", ATTRS{{idProduct}}=="{idProduct}", '
+        f'SYMLINK+="{name}"\n'
+    )
     return rule
 
 def write_udev_rules(rules):
@@ -36,17 +27,12 @@ def reload_udev_rules():
     subprocess.run(['sudo', 'udevadm', 'trigger'], check=True)
 
 def main():
-    # Ensure this script is running with superuser privileges
     if os.geteuid() != 0:
-        exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
+        exit("You need root privileges to run this script.")
 
-    # Create udev rules for all devices in the dictionary
     udev_rules = [create_udev_rule(name, **attributes) for name, attributes in DEVICES_INFO.items()]
-    
-    # Write the udev rules to the file
-    write_udev_rules(udev_rules)
 
-    # Reload the udev rules to apply changes
+    write_udev_rules(udev_rules)
     reload_udev_rules()
     print("Udev rules created and loaded successfully.")
 
